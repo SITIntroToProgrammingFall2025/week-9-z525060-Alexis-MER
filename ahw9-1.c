ce4df2ea123c684ca4702f99ddb7e9e68b5d3e31
Atrex04 <alexis.mer@epitech.eu>
2025-12-09 07:26:52 +0100

    feat: advanced ex

--- ahw9-1.c
+++ ahw9-1.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
 
+int main(void) {
+    int colors[5][3] = {
+        {255, 0, 0},
+        {0, 255, 0},
+        {0, 0, 255},
+        {0, 0, 0},
+        {255, 255, 255}
+    };
+
+    char color_names[5][6] = {
+        "Red",
+        "Green",
+        "Blue",
+        "Black",
+        "White"
+    };
+
+    int input[3];
+
+    for (int i = 0; i < 3; i++) {
+        scanf("%d", &input[i]);
+    }
+    
+    int min_abs_diff = 999;
+    int min_index = -1;
+
+    for (int i = 0; i < 5; i++) {
+        int abs_diff = 0;
+        for (int j = 0; j < 3; j++) {
+            int diff = colors[i][j] - input[j];
+            if (diff < 0) {
+                diff = -diff;
+            }
+            abs_diff += diff;
+        }
+        if (abs_diff < min_abs_diff) {
+            min_abs_diff = abs_diff;
+            min_index = i;
+        }
+    }
+
+    printf("The nearest color is %s\n", color_names[min_index]);
+
+    return 0;
+}
--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main(void) {
+    int input[5];
+
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &input[i]);
+    }
+
+    int sorted[5];
+    for (int i = 0; i < 5; i++) {
+        sorted[i] = input[i];
+    }
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            if (sorted[i] > sorted[j]) {
+                int temp = sorted[i];
+                sorted[i] = sorted[j];
+                sorted[j] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", sorted[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
